b5d45609f82b94b228e32d579996c7fcbcb222c0
李海青 <lihaiqing@log56.com>
2019-12-10 11:26:42 +0800

    新增输出ibatys的sql语句

--- src/com/lhq/plugin/mybatis/logs/console/DocumentListener.java
+++ src/com/lhq/plugin/mybatis/logs/console/DocumentListener.java
@@ -13,9 +13,14 @@ import org.eclipse.jface.text.IDocumentListener;
  *
  */
 public class DocumentListener implements IDocumentListener {
-    private static final String preparing  = " ==>  Preparing: ";
-    private static final String parameters = " ==> Parameters: ";
+    // MyBatis
+    private static final String preparing1  = " ==>  Preparing: ";
+    private static final String parameters1 = " ==> Parameters: ";
     private static final String total      = " <==      Total: ";
+    // iBatis
+    private static final String preparing2  = " Executing Statement: ";
+    private static final String parameters2 = " Parameters: ";
+    private static final String types       = " Types: ";
 
     private Map<String, String> cache = new HashMap<>();
     private StringBuilder buffer = new StringBuilder();
@@ -38,38 +43,72 @@ public class DocumentListener implements IDocumentListener {
             if (line == null || "".equals(line)) {
                 continue;
             }
-            if (line.contains(preparing)) {
-                cache.put(preparing, line.split(preparing)[1].trim());
-            } else if (line.contains(parameters)) {
-                String[] params = line.split(parameters);
+            // MyBatis
+            if (line.contains(preparing1)) {
+                cache.put(preparing1, line.split(preparing1)[1].trim());
+            } else if (line.contains(parameters1)) {
+                String[] params = line.split(parameters1);
                 if (params.length > 1) {
-                    cache.put(parameters, line.split(parameters)[1].trim());
+                    cache.put(parameters1, line.split(parameters1)[1].trim());
                 } else {
-                    cache.put(parameters, "");
+                    cache.put(parameters1, "");
                 }
             } else if (line.contains(total)) {
                 cache.put(total, line.split(total)[1].trim());
-                this.printSql();
+                this.printMyBatisSql();
+                continue;
+            }
+            // iBatis
+            if (line.contains(preparing2)) {
+                cache.put(preparing2, line.split(preparing2)[1].trim());
+            } else if (line.contains(parameters2)) {
+                cache.put(parameters2, line.split(parameters2)[1].trim());
+            } else if (line.contains(types)) {
+                this.printIBatisSql();
             }
         }
     }
 
     /**
-     * 输出SQL
+     * 输出MyBatis SQL
      */
-    private void printSql() {
-        if (cache.get(preparing) != null && cache.get(parameters) != null && cache.get(total) != null) {
-            List<String> params = Arrays.asList(cache.get(parameters).split(","));
+    private void printMyBatisSql() {
+        if (cache.get(preparing1) != null && cache.get(parameters1) != null && cache.get(total) != null) {
+            List<String> params = Arrays.asList(cache.get(parameters1).split(","));
 
             for (String param : params) {
                 param = param.trim().replaceAll("\\(\\w+\\)", "");
-                cache.put(preparing, cache.get(preparing).replaceFirst("\\?", "'" + param + "'"));
+                cache.put(preparing1, cache.get(preparing1).replaceFirst("\\?", "'" + param + "'"));
             }
             buffer.setLength(0);
             buffer.append(this.consoleName + "\n");
-            buffer.append("   SQL:  " + cache.get(preparing) + "\n");
-            buffer.append("PARAMS:  " + cache.get(parameters) + "\n");
+            buffer.append("   SQL:  " + cache.get(preparing1).replaceAll(" +", " ") + "\n");
+            buffer.append("PARAMS:  " + cache.get(parameters1) + "\n");
             buffer.append(" TOTAL:  " + cache.get(total) + "\n");
+            buffer.append("------------------------------------------------------------------------\n");
+            CurrentConsole.println(buffer.toString());
+            cache.clear();
+        }
+    }
+
+    /**
+     * 输出iBatis SQL
+     */
+    private void printIBatisSql() {
+        if (cache.get(preparing2) != null && cache.get(parameters2) != null) {
+            String parameters = cache.get(parameters2);
+            parameters = parameters.substring(1, parameters.length() - 1);
+            List<String> params = Arrays.asList(parameters.split(","));
+
+            for (String param : params) {
+                param = param.trim().replaceAll("\\(\\w+\\)", "");
+                cache.put(preparing2, cache.get(preparing2).replaceFirst("\\?", "'" + param + "'"));
+            }
+            buffer.setLength(0);
+            buffer.append(this.consoleName + "\n");
+            buffer.append("   SQL:  " + cache.get(preparing2).replaceAll(" +", " ") + "\n");
+            buffer.append("PARAMS:  " + cache.get(parameters2) + "\n");
+            buffer.append(" TOTAL:  \n");
             buffer.append("------------------------------------------------------------------------\n");
             CurrentConsole.println(buffer.toString());
             cache.clear();
